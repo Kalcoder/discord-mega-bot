e1c7c78d93bf52f7518dcfcb3c59c5c0c225a4e2
Kalcoder <kalebr82@kprschools.ca>
2019-06-21 13:48:57 -0400

    Add .gitignore

--- bot.js
+++ bot.js
@@ -1,11 +1,18 @@
 const Discord = require('discord.js')
 
+const handlers = require('./handlers')
+
 const auth = require('./credentials.json')
 
-const bot = new Discord.Client({
-  token: auth.token
-})
+const bot = new Discord.Client()
+bot.login(auth.token)
+
+console.log("Hello!")
 
 bot.on("ready", () => {
   console.log("Ready to go!")
+})
+
+bot.on("message", (message) => {
+  console.log(message.content)
 })
